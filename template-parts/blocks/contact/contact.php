4e1ea4b1a81ff8cb534e7c9313b9e6a917468c65
cyrilsource <cyrilbron78@gmail.com>
2019-06-14 17:17:30 +0200

    Update contact.php

--- template-parts/blocks/contact/contact.php
+++ template-parts/blocks/contact/contact.php
@@ -17,70 +17,66 @@ $to = get_field('to');
 global $wp;
 $current_slug = add_query_arg( array(), $wp->request );
 
-?>
-<div id="contactAlert" >
+if (isset($_GET['success'])) {
+	$datas = $_GET['success']; ?>
+	<div id="contactAlertSuccess">
+	</div>
+	<div class="alert-message success">
+		<span><?php echo $message_succes; ?></span>
+	</div>
+<?php }
 
-	<?php if (isset($_GET['success'])) {
-	  $datas = $_GET['success']; ?>
-	  <div class="alert-message success">
-	    <span><?php echo $message_success; ?></span></br>
-	  </div>
+if(array_key_exists('errors', $_SESSION)):
+	if (isset($_GET['errors'])) {
+	$datas = $_GET['errors']; ?>
+	<div id="contactAlertError">
+	</div>
+	<div class="alert-message error">
+		<span><?php echo $datas; ?></span>
+	</div>
 	<?php }
 
-	if(array_key_exists('errors', $_SESSION)):
-	  if (isset($_GET['errors'])) {
-	    $datas = $_GET['errors']; ?>
-	    <div class="alert-message error">
-	      <span><?php echo $datas; ?></span>
-	    </div>
-	  <?php }
-
-	endif; ?>
-
-</div>
+endif; ?>
 
 <div class="form1">
-
-  <?php if( have_rows('input') ): ?>
-    <form action="#" id="contactForm" method="post">
-    <?php wp_nonce_field('contact-form', 'contact-verif');
-    // loop through the rows of data
-    while ( have_rows('input') ) : the_row();
-	  // display a sub field value
-	  $label = get_sub_field('label');
-
-            switch ($label) {
-              case 'name': ?>
-                <label for="name">name</label>
-            <input type="text" name="name" id="name" value="<?= isset($_SESSION['input']['name']) ? $_SESSION['input']['name']:'';   ?>" /><?php
-                break;
-               case 'email': ?>
-                <label for="email">email</label>
-            <input type="email" name="email" id="email" value="<?= isset($_SESSION['input']['email']) ? $_SESSION['input']['email']:'';   ?>" /><?php
-                break;
-              case 'phone': ?>
-                <label for="phone">phone</label>
-                <input type="text" name="phone" id="phone" value="<?= isset($_SESSION['input']['phone']) ? $_SESSION['input']['phone']:'';   ?>" /><?php
-                break;
-               case 'url': ?>
-                <label for="url">url</label>
-                <input type="url" name="url" id="url" value="<?= isset($_SESSION['input']['url']) ? $_SESSION['input']['url']:'';   ?>" /><?php
-                break;
-               case 'subject': ?>
-                <label for="subject">subject</label>
-                <input type="text" name="subject" id="subject" value="<?= isset($_SESSION['input']['subject']) ? $_SESSION['input']['subject']:'';   ?>" /><?php
-                break;
-              default: ?>
-                <label for="subject">Message</label>
-        <textarea name="message" id="message" rows="20" cols="30"><?= isset($_SESSION['input']['message']) ? $_SESSION['input']['message']:'';   ?></textarea><?php
-                break;
-            }
-
-      endwhile; ?>
-      <button id="submit" type="submit"><?php echo $text_bouton_envoi; ?></button>
-      <input type="hidden" name="to" id="to" value="<?php echo $to; ?>" />
-      <input type="hidden" name="current_slug" id="current_slug" value="<?php echo $current_slug; ?>" />
-  </form>
+	<?php if( have_rows('input') ): ?>
+    	<form action="#" id="contactForm" method="post">
+    	<?php wp_nonce_field('contact-form', 'contact-verif');
+    	// loop through the rows of data
+    	while ( have_rows('input') ) : the_row();
+		// display a sub field value
+		$label = get_sub_field('label');
+		switch ($label) {
+		case 'name': ?>
+		<label for="name">name</label>
+		<input type="text" name="name" id="name" value="<?= isset($_SESSION['input']['name']) ? $_SESSION['input']['name']:'';   ?>" /><?php
+		break;
+		case 'email': ?>
+		<label for="email">email</label>
+		<input type="email" name="email" id="email" value="<?= isset($_SESSION['input']['email']) ? $_SESSION['input']['email']:'';   ?>" /><?php
+		break;
+		case 'phone': ?>
+		<label for="phone">phone</label>
+		<input type="text" name="phone" id="phone" value="<?= isset($_SESSION['input']['phone']) ? $_SESSION['input']['phone']:'';   ?>" /><?php
+		break;
+		case 'url': ?>
+		<label for="url">url</label>
+		<input type="url" name="url" id="url" value="<?= isset($_SESSION['input']['url']) ? $_SESSION['input']['url']:'';   ?>" /><?php
+		break;
+		case 'subject': ?>
+		<label for="subject">subject</label>
+		<input type="text" name="subject" id="subject" value="<?= isset($_SESSION['input']['subject']) ? $_SESSION['input']['subject']:'';   ?>" /><?php
+		break;
+		default: ?>
+		<label for="subject">Message</label>
+		<textarea name="message" id="message" rows="20" cols="30"><?= isset($_SESSION['input']['message']) ? $_SESSION['input']['message']:'';   ?></textarea><?php
+		break;
+		}
+	endwhile; ?>
+      	<button id="submit" type="submit"><?php echo $text_bouton_envoi; ?></button>
+      	<input type="hidden" name="to" id="to" value="<?php echo $to; ?>" />
+      	<input type="hidden" name="current_slug" id="current_slug" value="<?php echo $current_slug; ?>" />
+ 	</form>
 </div>
 <?php else :
 
@@ -96,6 +92,11 @@ unset($_SESSION['input']);
 * {
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
+}
+	
+#contactAlertError,
+#contactAlertSuccess {
+  height: 150px;
 }
 
 .form1 input[type=text], select, textarea {
